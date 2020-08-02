f91ee25e6695b75d0443de4af554b81f4a4e387a
jedi <amajid@nodeflux.io>
2020-08-02 20:09:29 +0700

    include basic evaluator in training loop

--- train.py
+++ train.py
@@ -4,6 +4,9 @@ from model import *
 import random
 import time
 import math
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from pathlib import Path
 
 n_hidden = 128
 n_epochs = 100000
@@ -25,6 +28,14 @@ def randomTrainingPair():
     category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
     line_tensor = Variable(lineToTensor(line))
     return category, line, category_tensor, line_tensor
+
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
 
 rnn = RNN(n_letters, n_hidden, n_categories)
 optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
@@ -57,11 +68,14 @@ def timeSince(since):
 
 start = time.time()
 
+n_confusion = 10000
+
 for epoch in range(1, n_epochs + 1):
     category, line, category_tensor, line_tensor = randomTrainingPair()
     output, loss = train(category_tensor, line_tensor)
     current_loss += loss
 
+    
     # Print epoch number, loss, name and guess
     if epoch % print_every == 0:
         guess, guess_i = categoryFromOutput(output)
@@ -72,6 +86,38 @@ for epoch in range(1, n_epochs + 1):
     if epoch % plot_every == 0:
         all_losses.append(current_loss / plot_every)
         current_loss = 0
+
+    confusion = torch.zeros(n_categories, n_categories)
+    # Go through a bunch of examples and record which are correctly guessed
+    for i in range(n_confusion):
+        category, line, category_tensor, line_tensor = randomTrainingPair()
+        output = evaluate(line_tensor)
+        guess, guess_i = categoryFromOutput(output)
+        category_i = all_categories.index(category)
+        confusion[category_i][guess_i] += 1
+
+    # Normalize by dividing every row by its sum
+    for i in range(n_categories):
+        confusion[i] = confusion[i] / confusion[i].sum()
+
+    # Set up plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(confusion.numpy())
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + all_categories, rotation=90)
+    ax.set_yticklabels([''] + all_categories)
+
+    # Force label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    # sphinx_gallery_thumbnail_number = 2
+    fig_path = f'out/{epoch}.jpg'
+    Path(fig_path).parent.mkdir(parents=True, exist_ok=True)
+    plt.savefig(fig_path)
 
 torch.save(rnn, 'char-rnn-classification.pt')
 
